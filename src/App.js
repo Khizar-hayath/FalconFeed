8ded95c7573d30393867eefa7634e61cf3b5102c
Khizar <khizerhayathakbar@gmail.com>
2025-02-20 22:55:00 +0530

    Basic structure of Website

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ export default class App extends Component {
     return (
       <div>
         <Navbar/>
-        Hey how is it going on? 
         <News/>
       </div>
     )
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title, description, imageurl, newsUrl} = this.props;
     return (
-      <div>
-        This is a newsItem
+      <div className='my-3'>
+        <div className="card" style={{width: "18rem"}}>
+          <img src={imageurl} className="card-img-top" alt="image"/>
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsdetail/newsId" className="btn btn-sm btn-primary">Read More</a>
+          </div>
+        </div>
       </div>
     )
   }
