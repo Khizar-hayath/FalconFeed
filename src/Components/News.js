39a09257231afcfe5b7fef7fd7a3650f57aa218d
Khizar <khizerhayathakbar@gmail.com>
2025-02-22 11:49:51 +0530

    Adding loading bar, next and previous button and other formatting in website

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,34 +1,78 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
+import Spinner from './Spinner';
 
 export class News extends Component {
   constructor(){
     super();
-    console.log("I am a constructor");
     this.state={
       articles: [],
-      loading: false 
+      loading: false,
+      page: 1, 
     };
   }
 
+
   async componentDidMount(){
-    console.log("ComponentDidMount");
-    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=acf658acc68b4218bd87ef623db45cab";
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=acf658acc68b4218bd87ef623db45cab&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true});
     let data = await fetch(url);
     let parsedData = await data.json()
-    console.log(parsedData);
-    this.setState({ articles: parsedData.articles });
+    this.setState({ articles : parsedData.articles,
+      totalArticles : parsedData.totalResults,
+      loading: false
+    });
   }
+
+
+  handlePrevClick = async ()=> {
+    console.log("Previous");
+    this.setState({loading: true});
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=acf658acc68b4218bd87ef623db45cab&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+      loading: false
+    })
+  }
+
+
+  handleNextClick = async ()=> {
+    console.log("Next");
+    if(!(this.state.page+1 > Math.ceil(this.state.totalArticles/this.props.pageSize))){
+      this.setState({loading: true});
+      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=acf658acc68b4218bd87ef623db45cab&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+      let data = await fetch(url);
+      let parsedData = await data.json()
+  
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+        loading: false
+      })
+    }
+  }
+
 
   render() {
     return (
       <div className='container'>
-        <h2>NewsMonkey - Top Headlines</h2>  {/* NewsDuck ?? */}
+        <h1 className='text-center'>NewsMonkey - Top Headlines</h1>  {/* NewsDuck ?? */}
+        {this.state.loading && <Spinner/>}
         <div className="row">
-        {this.state.articles.map((element)=> {
+          
+        {!this.state.loading && this.state.articles.map((element)=> {
           return <div className="col-md-4" key={element.url}>
-          <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageurl={element.urlToImage} newsUrl={element.url}/></div>
+          <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageurl={element.urlToImage} newsUrl={element.url}/>
+          </div>
         })}
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button disabled={this.state.page+1 > Math.ceil(this.state.totalArticles/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
